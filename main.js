cce1e75003b34ff35acc2dd7e52fcd5503e175a3
Cyuzuzo Bienvenu <cbienvenu007@gmail.com>
2023-12-18 18:49:19 +0200

    complete playRound function: compare player and computer moves, log the result

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
-alert('hello');
+function getComputerChoice () {
+ const randomNum = Math.floor(Math.random() * 3) + 1;
+//  console.log(randomNum);
+
+ if (randomNum === 1) {
+    return 'rock';
+ } else if (randomNum === 2) {
+    return 'paper';
+ } else if (randomNum === 3) {
+    return 'scissors';
+ }
+
+}
+
+function playRound(playerSelection, computerSelection) {
+const playerChoice = playerSelection;
+// console.log(playerChoice); 
+
+if (playerChoice === 'rock') {
+    if (computerSelection === 'rock') {
+        console.log(`Tie! ${playerChoice} and ${computerSelection}`);
+       
+    } else if (computerSelection === 'paper') {
+        console.log(`You lose! ${computerSelection} beats ${playerChoice}`);
+    } else if (computerSelection === 'scissors') {
+        console.log(`You win! ${playerChoice} beats ${computerSelection}`);
+    }
+} else if (playerChoice === 'paper') {
+    if (computerSelection === 'rock') {
+        console.log(`You win! ${playerChoice} beats ${computerSelection}`);
+    } else if (computerSelection === 'paper') {
+        console.log(`Tie! ${computerSelection} and ${playerChoice}`);
+    } else if (computerSelection === 'scissors') {
+        console.log(`You lose! ${computerSelection} beats ${playerChoice}`);
+    }
+} else if (playerChoice === 'scissors') {
+    if (computerSelection === 'sccissors') {
+        console.log(`Tie! ${playerChoice} and ${computerSelection}`);
+    } else if (computerSelection === 'rock') {
+        console.log(`You lose! ${computerSelection} beats ${playerChoice}`);
+    } else if (computerSelection === 'paper') {
+        console.log(`You win! ${playerChoice} beats ${computerSelection}`);
+    }
+}
+}
+
+let playerSelection = prompt('Your move:');
+playerSelection = playerSelection.toLowerCase();
+const computerSelection = getComputerChoice();
+
+
+console.log(playRound(playerSelection, computerSelection));
+
