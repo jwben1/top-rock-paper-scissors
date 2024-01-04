1521555911736b927456ae400cc08f76aa0d65c8
Cyuzuzo Bienvenu <cbienvenu007@gmail.com>
2024-01-04 17:58:10 +0200

    add disableBtn function & count score in playGame fn
    disableBtn: disable buttons when a score of 5 is reached
    count: checks if the score has reached to 5 and initialize the disableBtn

--- main.js
+++ main.js
@@ -4,18 +4,46 @@ let playerScore = 0;
 let playerMove;
 
 const rockBtn = document.querySelector('.rock-btn');
+const paperBtn = document.querySelector('.paper-btn');
+const scissorsBtn = document.querySelector('.scissors-btn');
+
+
 rockBtn.addEventListener('click', () => {
     playGame('rock');
 });
+paperBtn.addEventListener('click', () => {
+    playGame('paper');
+});
+scissorsBtn.addEventListener('click', () => {
+    playGame('scissors');
+});
+
 
 function getcomputerMove() {
  const moves = ['rock','paper','scissors'];
 return moves[Math.floor(Math.random() * moves.length)];
 }
 
+function disableBtn() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    
+}
+
 function playGame(playerMove) {
  const computerMove = getcomputerMove();
 
+ if (computerScore >= 5) {
+    console.log('You lost');
+    disableBtn();
+    return;
+ } else if (playerScore >= 5) {
+    console.log('You win');
+    disableBtn();
+    return;
+ }
+ 
 if (playerMove === 'rock' && computerMove === 'paper' ||
     playerMove === 'paper' && computerMove === 'scissors' ||
     playerMove === 'scissors' && computerMove === 'rock') {
